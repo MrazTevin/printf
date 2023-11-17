8a1f85307525a84ac6459220a7d1a91ac6bd35e2
MrazTevin <millatevin@gmail.com>
2023-11-17 13:39:40 +0000

    m: modify printf function

--- _printf.c
+++ _printf.c
@@ -1,5 +1,3 @@
-/* printf_functions.c */
-
 #include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
@@ -14,8 +12,6 @@ int _printf(const char *format, ...)
 {
 va_list args;
 int count = 0;
-char buffer[1024];  /* Local buffer */
-int buffer_index = 0;  /* Index to track the current position in the buffer */
 
 va_start(args, format);
 
@@ -25,7 +21,7 @@ va_end(args);
 return (-1);
 }
 
-count = process_format_string(format, args, buffer, &buffer_index);
+count = process_format_string(format, args);
 
 va_end(args);
 return (count);
@@ -35,48 +31,25 @@ return (count);
 * process_format_string - Process the format string
 * @format: Format string
 * @args: Variable arguments list
-* @buffer: Buffer for storing formatted output
-* @buffer_index: Index to track the current position in the buffer
 *
 * Return: Number of characters printed
 */
-int process_format_string(const char *format, va_list args,
-char *buffer, int *buffer_index)
+int process_format_string(const char *format, va_list args)
 {
 int count = 0;
 const char *ptr;
-print_t print_arr[] = {
-{'c', print_char},
-{'s', print_str},
-{'%', print_percent},
-{'d', print_int},
-{'i', print_int},
-{'u', print_unsigned},
-{'o', print_octal},
-{'x', print_hex_lower},
-{'X', print_hex_upper},
-{0, NULL}
-};
 
 for (ptr = format; *ptr != '\0'; ptr++)
 {
-if (*ptr == '%')
+if (*ptr == '%' && *(ptr + 1) != '\0')
 {
 ptr++; /* Move to the next character after '%' */
-count += process_percent_specifier(ptr, print_arr, args, buffer, buffer_index);
+count += process_percent_specifier(ptr, args);
 }
 else
 {
-count += process_regular_char(*ptr, buffer, buffer_index);
+count += write(1, ptr, 1);
 }
-}
-
-/* Print any remaining characters in the buffer */
-if (*buffer_index > 0)
-{
-write(1, buffer, *buffer_index);
-count += *buffer_index;
-*buffer_index = 0; /* Reset the buffer index */
 }
 
 return (count);
@@ -85,49 +58,33 @@ return (count);
 /**
 * process_percent_specifier - Process a percent specifier in the format string
 * @specifier: The percent specifier character
-* @print_arr: Array of print functions
 * @args: Variable arguments list
-* @buffer: Buffer for storing formatted output
-* @buffer_index: Index to track the current position in the buffer
 *
 * Return: Number of characters printed
 */
-int process_percent_specifier(const char *specifier, print_t *print_arr,
-va_list args, char *buffer, int *buffer_index)
+int process_percent_specifier(const char *specifier, va_list args)
 {
 int count = 0;
 
-for (int i = 0; print_arr[i].spec != 0; i++)
+switch (*specifier)
 {
-if (*specifier == print_arr[i].spec)
-{
-/* Check if the buffer is full before calling the function */
-if (*buffer_index >= 1023)
-{
-write(1, buffer, *buffer_index);
-count += *buffer_index;
-*buffer_index = 0; /* Reset the buffer index */
-}
-
-/* Call the corresponding print function and update count and buffer_index */
-count += print_arr[i].func(args, buffer + *buffer_index);
-*buffer_index += count;
-
+case 'c':
+count += print_char(va_arg(args, int));
 break;
-}
+case 's':
+count += print_str(va_arg(args, char *));
+break;
+case '%':
+count += print_percent();
+break;
+default:
+/* Handle unsupported specifier */
+count += write(1, "%", 1) + write(1, specifier, 1);
+break;
 }
 
 return (count);
 }
 
-/**
-* process_regular_char - Process a regular character in the format string
-* @c: The character to process
-* @buffer: Buffer for storing formatted output
-* @buffer_index: Index to track the current position in the buffer
-*
-* Return: Number of characters printed
-*/
-int process_regular_char(char c, char *buffer, int *buffer_index)
-{
+/* Implement print_char, print_str, and print_percent functions here */
 
